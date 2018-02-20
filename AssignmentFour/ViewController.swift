e67c1ba3e05ab903cde94f123aec3e227dd1a480
Дана Жолдыхаирова <danazholdykhairova@macbook-pro-dana.local>
2018-02-21 02:47:52 +0600

    Initial Commit

---
+++ AssignmentFour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssignmentFour
+//
+//  Created by Dana  Zholdykhairova on 2/21/18.
+//  Copyright © 2018 Dana  Zholdykhairova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
